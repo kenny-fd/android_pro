035a3f6ee6e83868838946a3c1c61d0383d34b0b
kenny <kenny>
2015-06-19 10:48:01 +0800

    java

---
+++ WXEntryActivity.java
@@ -0,0 +1,72 @@
+package cn.warthog.playercommunity.wxapi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import cn.warthog.playercommunity.R;
+import cn.warthog.playercommunity.legacy.common.share.AbstractSharePlatform;
+import cn.warthog.playercommunity.legacy.common.share.ShareCallback;
+import cn.warthog.playercommunity.legacy.common.share.SharePlatformFactory;
+
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler{
+    
+    private IWXAPI mWXAPI;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.page_loading);
+        mWXAPI = WXAPIFactory.createWXAPI(this, "warthog", false);
+        Intent intent = getIntent();
+        mWXAPI.handleIntent(intent, this);
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        mWXAPI.handleIntent(intent, this);
+    }
+
+
+    @Override
+    public void onReq(BaseReq arg0) {
+        // 微信发送请求到第三方应用时，会回调到该方法
+    }
+
+
+    @Override
+    public void onResp(BaseResp resp) {
+        // 第三方应用发送到微信的请求处理后的响应结果，会回调到该方法
+        int result;
+        switch (resp.errCode) {
+        case BaseResp.ErrCode.ERR_OK:
+            result = ShareCallback.SHARE_SUCCESS;
+            break;
+        case BaseResp.ErrCode.ERR_USER_CANCEL:
+            result = ShareCallback.SHARE_CANCEL;
+            break;
+        case BaseResp.ErrCode.ERR_AUTH_DENIED:
+            result = ShareCallback.SHARE_AUTH_DENIED;
+            break;
+        default:
+            result = ShareCallback.SHARE_FAILED;
+            break;
+        }
+        
+        AbstractSharePlatform wxPlatform = SharePlatformFactory.sLastUsedWXSharePlatform;
+        if(wxPlatform != null) {
+            wxPlatform.onActivityResult(0, result, null);
+        }
+        
+        finish();
+    }
+
+}
